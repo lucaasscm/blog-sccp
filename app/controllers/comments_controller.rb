437e2934aa09fb3617e482b545806a74f97ab037
Lucas Camargo <lucas98martelli@hotmail.com>
2026-04-16 21:05:31 -0300

    Edit and remove comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,14 @@
 # frozen_string_literal: true
 
 class CommentsController < ApplicationController
+  before_action :set_post
+  before_action :set_comment, only: %i[edit update destroy]
+  before_action :require_owner, only: %i[edit update destroy]
+
+  def edit
+  end
+
   def create
-    @post = Post.published.find(params[:post_id])
     @comment = @post.comments.new(comment_params)
     @comment.user = Current.user
 
@@ -13,7 +19,42 @@ class CommentsController < ApplicationController
     end
   end
 
+  def update
+    if @comment.update(comment_params)
+      redirect_to @post, notice: "Comentário atualizado."
+    else
+      redirect_to @post, alert: "Não foi possível atualizar o comentário."
+    end
+  end
+
+  def destroy
+    @comment.destroy
+    respond_to do |format|
+      format.turbo_stream do
+          render turbo_stream: [
+            turbo_stream.remove("comment_#{@comment.id}"),
+            turbo_stream.update("comments-count", "(#{@post.comments.count})")
+          ]
+        end
+      format.html { redirect_to @post, notice: "Comentário excluído." }
+    end
+  end
+
   private
+
+  def set_post
+    @post = Post.published.find(params[:post_id])
+  end
+
+  def set_comment
+    @comment = @post.comments.find(params[:id])
+  end
+
+  def require_owner
+    unless @comment.user == Current.user
+      redirect_to @post, alert: "Você não tem permissão para isso."
+    end
+  end
 
   def comment_params
     params.require(:comment).permit(:body)
